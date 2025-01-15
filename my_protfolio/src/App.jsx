6463d54b5369b44851bcf282136f79bc0c70d730
Raghuveer Sharma <sharmaraghu157@gmail.com>
2025-01-15 14:58:17 +0530

    use react router dom

--- my_protfolio/src/App.jsx
+++ my_protfolio/src/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./Screens/About";
+import Contact from "./Screens/Contact";
+import Projects from "./Screens/Projects";
+import Skills from "./Screens/Skills";
+import Home from "./Screens/Home";
 
 const App = () => {
   return (
-    <div className="">
-      <Header />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Skills" element={<Skills />} />
+      </Routes>
+    </Router>
   );
 };
 
