905ebf226502b773b79ea98b287a38983ce1d945
Raghuveer Sharma <sharmaraghu157@gmail.com>
2025-01-21 12:30:03 +0530

    remove dead code change styling

--- my_protfolio/src/components/Card.jsx
+++ my_protfolio/src/components/Card.jsx
@@ -1,14 +1,9 @@
 import React from "react";
-import BookStore from "../assets/BookStore.png";
-import Todo from "../assets/Todo.png";
-import TextUtil from "../assets/TextUtil.png";
-import PassOp from "../assets/PassOp.png";
-import Portfolio from "../assets/Portfolio.png";
 
 const Card = ({ item }) => {
   return (
     <div>
-      <div className="card lg:card-side bg-[#ffffff] shadow-2xl hover:shadow-slate-700 md:w-[100%] w-72 rounded-md text-slate-700 border-2 border-[#dddef0] hover:scale-105 duration-500 shadow-cyan-100  ">
+      <div className="card lg:card-side bg-[#ffffff] shadow-2xl hover:shadow-slate-700 md:w-[100%] w-72 rounded-md text-slate-700 border-2 border-[#dddef0] hover:scale-105 duration-500 shadow-cyan-600  ">
         <figure>
           <img src={item.image} alt={item.title} className="w-fit h-72" />
         </figure>
