5c0c9bd437c19f2fde5fb77bae0220cadeb4d331
Your Name <sharmaraghu157@gmail.com>
2025-03-10 14:10:08 +0530

    add new project and type animation .

--- my_protfolio/src/Screens/Projects.jsx
+++ my_protfolio/src/Screens/Projects.jsx
@@ -6,7 +6,10 @@ import BookStore from "../assets/BookStore.png";
 import Todo from "../assets/Todo.png";
 import TextUtil from "../assets/TextUtil.png";
 import PassOp from "../assets/PassOp.png";
+import eco from "../assets/eco.png";
 import Portfolio from "../assets/Portfolio.png";
+
+import { TypeAnimation } from "react-type-animation";
 
 const Projects = () => {
   const projects = [
@@ -55,32 +58,53 @@ const Projects = () => {
       GitHub: "https://github.com/raghuveeersharma/TODO_FRONTEND",
       more: "",
     },
+    {
+      id: 6,
+      title: "EcoRecycle",
+      description:
+        "EcoRecycle is an intelligent web application designed to promote sustainable waste management by leveraging AI-powered object detection and location-based recycling center recommendations. ",
+      image: [eco],
+      GitHub: "https://github.com/raghuveeersharma/EcoRecycle",
+      more: "",
+    },
   ];
 
   return (
     <>
       <section
-        className="relative bg-cover bg-fixed bg-[#edf2f7] bg-center h-full md:max-h-full w-full overflow-x-hidden pb-20"
+        className="relative bg-cover bg-center bg-fixed h-full w-full overflow-x-hidden pb-20"
+        style={{
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1485470733090-0aae1788d5af?q=80&w=1517&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+        }}
+        // className="relative bg-cover bg-fixed bg-[#edf2f7] bg-center h-full md:max-h-full w-full overflow-x-hidden pb-20"
         id="Projects"
       >
-        <div className="absolute inset-0  opacity-55 items-center"></div>
+        <div className="absolute inset-0 bg-black  opacity-75 items-center"></div>
         <div className="relative z-10 md:max-w-4xl mx-auto text-center py-32 items-center max-w-2xl">
           <h1 className=" text-xl font-bold text-pink-700 mb-2">PROJECTS</h1>
-          <h1 className=" md:text-4xl text-2xl text-slate-800 font-bold">
-            Check out some of my works.
-          </h1>
-          <p className="mt-5 lg:px-0 px-4 text-slate-600 font-serif">
+          <span className="lg:text-5xl md:text-3xl text-2xl font-normal text-slate-400 mb-4 border-r-2 ">
+            <TypeAnimation
+              sequence={[
+                "Check out some of my works.",
+                2500, // wait 1s before replacing "Mice" with "Hamsters"
+                "",
+                2500,
+              ]}
+              wrapper="span"
+              speed={20}
+              style={{ fontSize: "1em", display: "inline-block" }}
+              repeat={Infinity}
+            />
+          </span>
+          <p className="mt-5 lg:px-0 px-4 text-slate-300 font-serif">
             Projects and Technologies - Throughout my academic & professional
             journey, I have worked on various projects that demonstrate my
             expertise in modern technologies. My projects primarily utilize the
             MERN stack (MongoDB, Express.js, React.js, Node.js), showcasing my
             ability to create full-stack applications. I have also leveraged
             Vite for fast & efficient development, and Tailwind CSS to craft
-            responsive and visually appealing user interfaces. My projects often
-            incorporate tools like React Hot Toast, UUID, and React animations
-            for an enhanced user experience. These technologies, combined with
-            my problem-solving approach, have enabled me to develop innovative &
-            functional web applications tailored to user needs.
+            responsive and visually appealing user interfaces.
           </p>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 md:mx-20 mx-[14.5%] gap-16 my-10 items-center ">
